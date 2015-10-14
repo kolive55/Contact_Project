ec9260e9536ae5532d5e1b201984fcd010739710
Kyria <kyria.brown@gmail.com>
2015-10-13 19:32:20 -0700

    Update functionality

--- contact.rb
+++ contact.rb
@@ -1,6 +1,5 @@
-require_relative 'contact_database'
 require 'pg'
-require 'pry'
+
 
 class Contact
 
@@ -30,12 +29,49 @@ class Contact
     CONN.exec_params(action, [firstname, lastname, email])
   end
 
-  def self.update(firstname, lastname, email)
-    @id
-    binding.pry
-    action = ('UPDATE contacts SET first_name=$1, last_name=$2, email=$3 WHERE id = @id; ')
-    CONN.exec_params(action, [firstname, lastname, email])
+  def self.destroy(id)
+    action = ('DELETE FROM contacts WHERE id=$1; ')
+    CONN.exec_params(action, [id])
   end
+
+  def self.find(id)
+    action = ('SELECT * FROM contacts WHERE id=$1; ')
+    results = CONN.exec_params(action, [id])
+    results.each do |contact|
+      p contact
+    end
+  end
+
+  def self.find_all_by_lastname(name)
+    action = ("SELECT * FROM contacts WHERE last_name ILIKE '%#{name}%'; ")
+    results = CONN.exec_params(action)
+    results.each do |contact|
+      p contact
+    end
+  end
+
+  def self.find_all_by_firstname(name)
+    action = ("SELECT * FROM contacts WHERE first_name ILIKE '%#{name}%'; ")
+    results = CONN.exec_params(action)
+    results.each do |contact|
+      p contact
+    end
+  end
+
+  def self.find_all_by_email(email)
+    action = ("SELECT * FROM contacts WHERE email ILIKE '%#{email}%'; ")
+    results = CONN.exec_params(action)
+    results.each do |contact|
+      p contact
+    end
+  end
+
+  # def self.update(firstname, lastname, email)
+  #   @id
+  #   binding.pry
+  #   action = ('UPDATE contacts SET first_name=$1, last_name=$2, email=$3 WHERE id = @id; ')
+  #   CONN.exec_params(action, [firstname, lastname, email])
+  # end
 
   def self.all
     results = CONN.exec_params("SELECT * FROM contacts;")
@@ -45,6 +81,3 @@ class Contact
   end
 
 end
-
-# Contact.all
-Contact.update('Oscar', 'Bluth', 'Oscar3@bluth.bluth')
